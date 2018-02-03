7afd3ca5baf179566e33ee11b2bd4040694c532f
Kohei Kakimoto <naige.1028@gmail.com>
2018-02-03 17:19:00 +0900

    Add new programming problem: Six chars world
    Decode JSFuck code to get FLAG

--- html/welcome/index.php
+++ html/welcome/index.php
@@ -2,7 +2,7 @@
 <p>
     これは練習用の問題です。下記のフラグ文字列を送信してください。
 </p>
-<p>
-    <code>FLAG{welcome2ctf4d}</code>
-</p>
+<pre>
+FLAG{welcome2ctf4d}
+</pre>
 <?php include(dirname(__FILE__) . '/../common/includes/problem_footer.inc.php'); ?>
