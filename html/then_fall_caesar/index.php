4b4dcbfac6b36e487b989f9eb19a64e8661478ab
Kohei Kakimoto <naige.1028@gmail.com>
2018-02-04 16:26:29 +0900

    Add new Crypt problem using Carsar cipher

---
+++ html/then_fall_caesar/index.php
@@ -0,0 +1,8 @@
+<?php include(dirname(__FILE__) . '/../common/includes/problem_header.inc.php'); ?>
+<p>
+    下記の暗号文を解読してフラグを入手してください。
+</p>
+<pre>
+VLR ZXK DBQ CIXD YV ZLKKBZQFKD QEOBB PQOFKDP, 'PF', 'WX', 'XKDL' TFQE EVMEBK (CIXD PQOFKD FP RMMBO ZXPB).
+</pre>
+<?php include(dirname(__FILE__) . '/../common/includes/problem_footer.inc.php'); ?>
