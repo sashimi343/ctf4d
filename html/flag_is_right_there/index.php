f1e951603e44c6c4182b857c345df343064c8d61
Kohei Kakimoto <naige.1028@gmail.com>
2018-02-06 23:36:46 +0900

    Add hints to problem 'Flag is right there!'

--- html/flag_is_right_there/index.php
+++ html/flag_is_right_there/index.php
@@ -2,4 +2,7 @@
 <p>
     FLAGは<a href="there.html">ここ</a>に置いてあります。
 </p>
+<p>
+    ページの内容を保存して、ゆっくり眺めればフラグが手に入りそうです。
+</p>
 <?php include(dirname(__FILE__) . '/../common/includes/problem_footer.inc.php'); ?>
