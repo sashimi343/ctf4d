999f0586d698f8df1a56a459bf3cbab50ee15b3e
Kohei Kakimoto <naige.1028@gmail.com>
2018-02-12 18:01:58 +0900

    Create dummy admin registration page

---
+++ html/priv_escalation/admin/register_page.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start(array('cookie_path' => '/priv_escalation'));
+
+?>
+<!DOCTYLE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Admin Register | CTF BBS</title>
+        <link rel="stylesheet" type="text/css" href="/priv_escalation/style.css" />
+    </head>
+    <body>
+        <h1>Admin Register</h1>
+        <p>This feature is disabled.</p>
+        <hr />
+        <a href="/priv_escalation/top.php">[Back]</a>
+    </body>
+</html>
